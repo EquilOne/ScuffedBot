ac2d4a28fb8bf6e34b2c929e5d0f9dfebf43ad4e
Chase <188378569+EquilOne@users.noreply.github.com>
2026-05-19 16:32:50 -0600

    feat(functions): add get_file_content utility and tests

---
+++ functions/get_file_content.py
@@ -0,0 +1,35 @@
+import os
+
+from config import MAX_CHARS
+
+
+def get_file_content(working_directory: str, file_path: str) -> str:
+    try:
+        working_dir_abs = os.path.abspath(working_directory)
+        target_file = os.path.normpath(os.path.join(working_dir_abs, file_path))
+        valid_target_file = (
+            os.path.commonpath([working_dir_abs, target_file]) == working_dir_abs
+        )
+
+        if not valid_target_file:
+            raise PermissionError(
+                f'Cannot read "{file_path}" as it is outside the permitted working directory'
+            )
+
+        if not os.path.isfile(target_file):
+            raise FileNotFoundError(
+                f'File not found or is not a regular file: "{file_path}"'
+            )
+
+        with open(target_file, "r") as f:
+            content = f.read(MAX_CHARS)
+            if f.read(1):
+                content += (
+                    f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+                )
+        return content
+
+    except PermissionError as e:
+        return f"Error: {e}"
+    except FileNotFoundError as e:
+        return f"Error: {e}"
